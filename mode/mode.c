ae431ddb4c1f3b3a42c1d51944b824a7d83846f4
Maciej Gajdzica <maciek@ucgosu.pl>
2024-01-05 15:01:03 +0100

    mode: add all fixed modes and bonus 1m/1s

--- mode/mode.c
+++ mode/mode.c
@@ -9,38 +9,19 @@
 
 static enum mode mode;
 
+static const struct config_controller * set_mode_fixed_1m(void);
+static const struct config_controller * set_mode_fixed_5m(void);
+static const struct config_controller * set_mode_fixed_15m(void);
+static const struct config_controller * set_mode_fixed_30m(void);
+static const struct config_controller * set_mode_fixed_1h(void);
+static const struct config_controller * set_mode_fixed_1h30min(void);
+
+static const struct config_controller * set_mode_bonus_1m_inc_1s(void);
+
 void mode_set(enum mode m)
 {
 	mode = m;
 }
-
-void set_mode_fixed_1m(void)
-{
-	const struct mode_builder *builder = mode_builder_get();
-
-	builder->init();
-	builder->set_time(PLAYER_BOTH, TIME_TO_MS(0, 1, 0));
-}
-
-static void empty_on_entry(config_completed_cb_t cb)
-{
-	cb();
-}
-
-static void empty_fun(void)
-{
-
-}
-
-const static struct config_controller config_empty = {
-	empty_on_entry,
-	empty_fun,
-	empty_fun,
-	empty_fun,
-	empty_fun,
-	empty_fun,
-	empty_fun,
-};
 
 const struct config_controller * mode_config_controller_get(void)
 {
@@ -59,8 +40,25 @@ const struct config_controller * mode_config_controller_get(void)
 			return fixed_control_config_controller_get();
 		
 		case FIXED_1M:
-			set_mode_fixed_1m();
-			return &config_empty;
+			return set_mode_fixed_1m();
+						
+		case FIXED_5M:
+			return set_mode_fixed_5m();
+				
+		case FIXED_15M:
+			return set_mode_fixed_15m();
+		
+		case FIXED_30M:
+			return set_mode_fixed_30m();
+						
+		case FIXED_1H:
+			return set_mode_fixed_1h();
+				
+		case FIXED_1H_30M:
+			return set_mode_fixed_1h30min();
+		
+		case BONUS_1M_1S:
+			return set_mode_bonus_1m_inc_1s();
 		
 		default:
 			//TODO: handle errors
@@ -73,9 +71,15 @@ const struct mode_builder * mode_builder_get(void)
 	switch (mode)
 	{
 		case FIXED_1M:
+		case FIXED_5M:
+		case FIXED_15M:
+		case FIXED_30M:
+		case FIXED_1H:
+		case FIXED_1H_30M:
 		case FIXED_CUSTOM:
 			return fixed_mode_builder_get();
 
+		case BONUS_1M_1S:
 		case BONUS_CUSTOM:
 			return bonus_mode_builder_get();
 
@@ -91,14 +95,20 @@ const struct mode_builder * mode_builder_get(void)
 	}
 }
 
-const struct game_controller *mode_game_controller_get(void)
+const struct game_controller * mode_game_controller_get(void)
 {
 	switch (mode)
 	{
 		case FIXED_1M:
+		case FIXED_5M:
+		case FIXED_15M:
+		case FIXED_30M:
+		case FIXED_1H:
+		case FIXED_1H_30M:
 		case FIXED_CUSTOM:
 			return fixed_game_controller_get();
 
+		case BONUS_1M_1S:
 		case BONUS_CUSTOM:
 			return bonus_game_controller_get();
 
@@ -112,4 +122,95 @@ const struct game_controller *mode_game_controller_get(void)
 			//TODO: handle errors
 			return NULL;
 	}
+}
+
+static void empty_on_entry(config_completed_cb_t cb)
+{
+	cb();
+}
+
+static void empty_fun(void)
+{
+
+}
+
+static const struct config_controller config_empty = {
+	empty_on_entry,
+	empty_fun,
+	empty_fun,
+	empty_fun,
+	empty_fun,
+	empty_fun,
+	empty_fun,
+};
+
+static const struct config_controller * set_mode_fixed_1m(void)
+{
+	const struct mode_builder *builder = mode_builder_get();
+
+	builder->init();
+	builder->set_time(PLAYER_BOTH, TIME_TO_MS(0, 1, 0));
+
+	return &config_empty;
+}
+
+static const struct config_controller * set_mode_fixed_5m(void)
+{
+	const struct mode_builder *builder = mode_builder_get();
+
+	builder->init();
+	builder->set_time(PLAYER_BOTH, TIME_TO_MS(0, 5, 0));
+
+	return &config_empty;
+}
+
+static const struct config_controller * set_mode_fixed_15m(void)
+{
+	const struct mode_builder *builder = mode_builder_get();
+
+	builder->init();
+	builder->set_time(PLAYER_BOTH, TIME_TO_MS(0, 15, 0));
+
+	return &config_empty;
+}
+
+static const struct config_controller * set_mode_fixed_30m(void)
+{
+	const struct mode_builder *builder = mode_builder_get();
+
+	builder->init();
+	builder->set_time(PLAYER_BOTH, TIME_TO_MS(0, 30, 0));
+
+	return &config_empty;
+}
+
+static const struct config_controller * set_mode_fixed_1h(void)
+{
+	const struct mode_builder *builder = mode_builder_get();
+
+	builder->init();
+	builder->set_time(PLAYER_BOTH, TIME_TO_MS(1, 0, 0));
+
+	return &config_empty;
+}
+
+static const struct config_controller * set_mode_fixed_1h30min(void)
+{
+	const struct mode_builder *builder = mode_builder_get();
+
+	builder->init();
+	builder->set_time(PLAYER_BOTH, TIME_TO_MS(1, 30, 0));
+
+	return &config_empty;
+}
+
+static const struct config_controller * set_mode_bonus_1m_inc_1s(void)
+{
+	const struct mode_builder *builder = mode_builder_get();
+
+	builder->init();
+	builder->set_time(PLAYER_BOTH, TIME_TO_MS(0, 1, 0));
+	builder->set_increment(PLAYER_BOTH, TIME_TO_MS(0, 0, 1));
+
+	return &config_empty;
 }
